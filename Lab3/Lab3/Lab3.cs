41ae0cbd74603b1c8d820af51854ee85123e7938
yana <janaak827@gmail.com>
2020-09-22 22:50:32 +0300

    lab3: problem 1,2

---
+++ Lab3/Lab3/Lab3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Lab3
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Person()
+            {
+                Name = "No name";
+                Age = 1;
+            }
+            public Person(int age)
+            {
+                Name = "No name";
+                Age = age;
+            }
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Console.WriteLine($"{p1.Name} - {p1.Age}");
+        }
+    }
+}
