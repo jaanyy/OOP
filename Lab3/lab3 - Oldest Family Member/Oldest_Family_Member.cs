a5d63fcbe9bc352923a1200680d2fad3527d6316
yana <janaak827@gmail.com>
2020-09-23 14:45:16 +0300

    added 3 problem

--- Lab3/lab3 - Oldest Family Member/Oldest_Family_Member.cs
+++ Lab3/lab3 - Oldest Family Member/Oldest_Family_Member.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab3
 {
@@ -24,10 +25,46 @@ namespace Lab3
                 Age = age;
             }
         }
+        class Family
+        {
+            private List<Person> members;
+            public Family()
+            {
+                members = new List<Person>();
+            }
+            public void AddMember (Person member)
+            {
+                members.Add(member);
+            }
+            public Person GetOldestMember()
+            {
+                int tempMaxAge = members[0].Age;
+                Person result = members[0];
+                foreach (var a in members)
+                {
+                    if (a.Age > tempMaxAge)
+                    {
+                        tempMaxAge = a.Age;
+                        result = a;
+                    }
+                }
+                return result;
+            }
+        }
         static void Main(string[] args)
         {
-            Person p1 = new Person();
-            Console.WriteLine($"{p1.Name} - {p1.Age}");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Family f1 = new Family();
+            for (int i = 0; i < N; ++i)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                Person person = new Person();
+                person.Name = input[0];
+                person.Age = Convert.ToInt32(input[1]);
+                f1.AddMember(person);
+            }
+            Person old = f1.GetOldestMember();
+            Console.WriteLine($"{old.Name} {old.Age}");
         }
     }
 }
