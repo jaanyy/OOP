2d8ba9b915031bbf32f03475978e3befaf2f88eb
jany <janaak827@gmail.com>
2020-10-21 22:02:21 +0300

    9

---
+++ Lab3/lab3_9-Rectangle_Intersection/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3_9_Rectangle_Intersection
+{
+    class Rectangle
+    {
+        public string Id { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public int LeftTopCorner_x { get; }
+        public int LeftTopCorner_y { get; }
+        public int CalculateRightBottomCorner_x()
+        {
+            return (LeftTopCorner_x + Width);
+        }
+        public int CalculateRightBottomCorner_y()
+        {
+            return (LeftTopCorner_y + Height);
+        }
+        public Rectangle(string id, int width, int height, int x, int y)
+        {
+            Id = id;
+            Width = width;
+            Height = height;
+            LeftTopCorner_y = y;
+            LeftTopCorner_x = x;
+        }
+        public bool CheckIntersection(Rectangle second)
+        {
+            var firstLeftTop_y = LeftTopCorner_y;
+            var firstLeftTop_x = LeftTopCorner_x;
+            var firstRightBottom_y = CalculateRightBottomCorner_y();
+            var firstRightBottom_x = CalculateRightBottomCorner_x();
+            var secondLeftTop_y = second.LeftTopCorner_y;
+            var secondLeftTop_x = second.LeftTopCorner_x;
+            var secondRightBottom_y = second.CalculateRightBottomCorner_y();
+            var secondRightBottom_x = second.CalculateRightBottomCorner_x();
+
+            return (secondRightBottom_x <= firstRightBottom_x && secondRightBottom_y >= firstRightBottom_y &&
+                    secondRightBottom_x >= firstLeftTop_x && secondRightBottom_y <= firstLeftTop_y) ||
+                   (secondLeftTop_x <= firstRightBottom_x && secondLeftTop_y >= firstRightBottom_y &&
+                    secondLeftTop_x >= firstLeftTop_x && secondLeftTop_y <= firstLeftTop_y);
+        }
+    }
+}
---
+++ Lab3/lab3_9-Rectangle_Intersection/Rectangle_Intersection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3_9_Rectangle_Intersection
+{
+    class Rectangle_Intersection
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(input[0]);
+            int m = Convert.ToInt32(input[1]);
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(' ');
+                rectangles.Add(new Rectangle(info[0], Convert.ToInt32(info[1]), Convert.ToInt32(info[2]), Convert.ToInt32(info[3]), Convert.ToInt32(info[4])));
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] info = Console.ReadLine().Split(' ');
+                Rectangle first = rectangles[0], second = rectangles[0];
+
+                Console.WriteLine(first.CheckIntersection(second));
+            }
+            Console.ReadKey();
+        }
+    }
+}
