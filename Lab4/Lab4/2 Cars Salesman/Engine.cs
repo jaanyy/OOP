926902fb996d917d68698372c59c314269bcb740
jany <janaak827@gmail.com>
2020-10-28 22:39:00 +0200

    L4 Cars Salesman

---
+++ Lab4/Lab4/2 Cars Salesman/Engine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_Cars_Salesman
+{
+    class Engine
+    {
+        public string Model { get; private set; }
+        private int power;
+        private int? displacement;
+        private string efficiency;
+
+        public Engine(string model, int power)
+        {
+            Model = model;
+            this.power = power;
+        }
+
+        public Engine(string model, int power, int? displacement)
+            : this(model, power)
+        {
+            this.displacement = displacement;
+        }
+
+        public Engine(string model, int power, string efficiency)
+            : this(model, power)
+        {
+            this.efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int? displacement, string efficiency)
+            : this(model, power)
+        {
+            this.displacement = displacement;
+            this.efficiency = efficiency;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"  {Model}:");
+            builder.AppendLine($"    Power: {power}");
+            builder.AppendLine($"    Displacement: {(displacement == null ? "n/a" : displacement.ToString())}");
+            builder.AppendLine($"    Efficiency: {(efficiency == null ? "n/a" : efficiency.ToString())}");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Lab4/Lab4/2 Cars Salesman/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_Cars_Salesman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < N; i++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = input[0];
+                int power = Convert.ToInt32(input[1]);
+
+                if (input.Length == 2)
+                {
+                    engines.Add(new Engine(model, power));
+                }
+                else if (input.Length == 3)
+                {
+                    if (input[2].All(Char.IsDigit))
+                    {
+                        int displacement = Convert.ToInt32(input[2]);
+                        engines.Add(new Engine(model, power, displacement));
+                    }
+                    else
+                    {
+                        string efficiency = input[2];
+                        engines.Add(new Engine(model, power, efficiency));
+                    }
+                }
+                else if (input.Length == 4)
+                {
+                    int displacement = Convert.ToInt32(input[2]);
+                    string efficiency = input[3];
+
+                    engines.Add(new Engine(model, power, displacement, efficiency));
+                }
+            }
+
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = input[0];
+                Engine engine = engines.First(e => e.Model == input[1]);
+
+                if (input.Length == 2)
+                {
+                    cars.Add(new Car(model, engine));
+                }
+                else if (input.Length == 3)
+                {
+                    if (input[2].All(Char.IsDigit))
+                    {
+                        int weight = Convert.ToInt32(input[2]);
+
+                        cars.Add(new Car(model, engine, weight));
+                    }
+                    else
+                    {
+                        string color = input[2];
+
+                        cars.Add(new Car(model, engine, color));
+                    }
+                }
+                else if (input.Length == 4)
+                {
+                    int weight = Convert.ToInt32(input[2]);
+                    string color = input[3];
+
+                    cars.Add(new Car(model, engine, weight, color));
+                }
+            }
+
+            cars.ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+
+        
+        }
+    }
+
