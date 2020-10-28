49eb31df1d733ea7d3be14438f3c2c750f32dfed
jany <janaak827@gmail.com>
2020-10-28 23:35:40 +0200

    L5 Animal Farm

---
+++ Lab5/Lab5/3 Animal Farm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_Animal_Farm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine("Chicken {0} (age {1}) can produce {2} eggs per day.", chicken.Name, chicken.Age, chicken.ProductPerDay);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
