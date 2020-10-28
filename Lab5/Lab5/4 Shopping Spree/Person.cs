2d29b2b2e9992be0afc929e0307ba04f1ec1b18f
jany <janaak827@gmail.com>
2020-10-28 23:46:02 +0200

    L5 Shopping Spree

---
+++ Lab5/Lab5/4 Shopping Spree/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_Shopping_Spree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        private decimal Money
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        public List<Product> Products
+        {
+            get
+            {
+                return products;
+            }
+        }
+
+        public bool BuyProduct(Product product)
+        {
+            if (money >= product.Cost)
+            {
+                money -= product.Cost;
+                products.Add(product);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Lab5/Lab5/4 Shopping Spree/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_Shopping_Spree
+{
+    class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                return cost;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                cost = value;
+            }
+        }
+    }
+}
