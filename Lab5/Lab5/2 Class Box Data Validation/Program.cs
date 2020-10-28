91022fce2c538f1bd39c53792a09f1dd768eec1a
jany <janaak827@gmail.com>
2020-10-28 23:11:23 +0200

    L5 Class Box Data Validation

---
+++ Lab5/Lab5/2 Class Box Data Validation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2_Class_Box_Data_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = Convert.ToDouble(Console.ReadLine());
+            double width = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine(box.GetSurfaceArea());
+                Console.WriteLine(box.GetLateralSurfaceArea());
+                Console.WriteLine(box.GetVolume());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
