6c5faf0b172ab8bc41bdd64a72fb738ac529251a
jany <janaak827@gmail.com>
2020-10-28 23:06:53 +0200

    L5 Class Box

---
+++ Lab5/Lab5/1 Class Box/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_Class_Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine(box.GetSurfaceArea());
+            Console.WriteLine(box.GetLateralSurfaceArea());
+            Console.WriteLine(box.GetVolume());
+            Console.ReadKey();
+        }
+    }
+}
