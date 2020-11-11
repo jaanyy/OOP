851a5e2e5f7044ce6bbff6a6caafb14062182fcf
jany <janaak827@gmail.com>
2020-11-11 23:12:31 +0200

    L6.1 Person

---
+++ Lab6/1 Person/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
