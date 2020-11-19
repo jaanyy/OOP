a635c259a5ebd369a89ee1a43e3a5cd74cdf7bee
jany <janaak827@gmail.com>
2020-11-19 09:17:44 +0200

    fix

--- Lab6/4.Online Radio Database/Program.cs
+++ Lab6/4.Online Radio Database/Program.cs
@@ -6,7 +6,25 @@ namespace _4.Online_Radio_Database
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = int.Parse(Console.ReadLine());
+            Playlist playlist = new Playlist();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(';');
+
+                try
+                {
+                    Song song = new Song(input);
+                    Console.WriteLine(playlist.AddSong(song));
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid song.");
+                }
+            }
+
+            Console.WriteLine(playlist);
         }
     }
 }
