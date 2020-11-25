c78f4d666a9530dfb743f24590d7642bfd7473da
jany <janaak827@gmail.com>
2020-11-25 21:18:27 +0200

    L7_4.Telephony

---
+++ Lab7/L7_4.Telephony/IBrowsable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L7_4.Telephony
+{
+    interface IBrowsable
+    {
+        string Browse(string url);
+    }
+}
---
+++ Lab7/L7_4.Telephony/ICallable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L7_4.Telephony
+{
+    public interface ICallable
+    {
+        string Call(string number);
+    }
+}
