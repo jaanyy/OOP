a924021df88fc4f17c3027b6a0eb7ac1dd98cdc2
jany <janaak827@gmail.com>
2020-11-25 19:02:20 +0200

    L8_3.Shapes

---
+++ Lab8/L8_3.Shapes/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L8_3.Shapes
+{
+    class Circle : Shape
+    {
+        const double Pi = 3.14;
+        public double Radius { get; private set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Radius * Radius * Pi;
+        }
+        public override double CalculatePerimeter()
+        {
+             return Radius * Pi * 2;
+        }
+        public override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
---
+++ Lab8/L8_3.Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L8_3.Shapes
+{
+    class Rectangle : Shape
+    {
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+        public override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+
+    }
+}
