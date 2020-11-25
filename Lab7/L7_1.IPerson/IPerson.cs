142da93cd11a60a306d4facc30fd92921acd2f0d
jany <janaak827@gmail.com>
2020-11-25 19:47:10 +0200

    L7_1.IPerson

---
+++ Lab7/L7_1.IPerson/IPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L7_1.IPerson
+{
+    interface IPerson
+    {
+        string Name { get; }
+        int Age { get; }
+    }
+}
