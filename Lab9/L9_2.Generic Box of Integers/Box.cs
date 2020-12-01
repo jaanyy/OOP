82a19d415d915745d1c7bc3f35ad053605401cc7
jany <janaak827@gmail.com>
2020-12-01 22:38:21 +0200

    L9_2.Generic Box of Integers

---
+++ Lab9/L9_2.Generic Box of Integers/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L9_2.Generic_Box_of_Integers
+{
+    class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}";
+        }
+    }
+}
---
+++ Lab9/L9_2.Generic Box of Integers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L9_2.Generic_Box_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                var box = new Box<int>(value);
+
+                Console.WriteLine(box);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
