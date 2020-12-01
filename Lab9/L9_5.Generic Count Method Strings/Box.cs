a20cc9db50e257c2c857a30275be93ad762796ac
jany <janaak827@gmail.com>
2020-12-01 23:58:59 +0200

    L9_5.Generic Count String

---
+++ Lab9/L9_5.Generic Count Method Strings/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L9_5.Generic_Count_Method_Strings
+{
+    class Box<T> : IComparable<T>
+    where T : IComparable<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public int CompareTo(T other)
+        {
+            return this.value.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}";
+        }
+    }
+}
---
+++ Lab9/L9_5.Generic Count Method Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L9_5.Generic_Count_Method_Strings
+{
+    class Program
+    {
+        static int CountGreater<T>(IEnumerable<Box<T>> collection, T element)
+        where T : IComparable<T>
+        {
+            int counter = 0;
+
+            foreach (var item in collection)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string value = Console.ReadLine();
+
+                boxes.Add(new Box<string>(value));
+            }
+
+            string element = Console.ReadLine();
+
+            Console.WriteLine(CountGreater(boxes, element));
+
+            Console.ReadKey();
+        }
+    }
+}
