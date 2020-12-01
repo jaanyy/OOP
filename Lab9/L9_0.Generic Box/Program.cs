569d99626b9f8d7076023b1260afc502088434ca
jany <janaak827@gmail.com>
2020-12-01 22:21:14 +0200

    L9_0.Generic Box

---
+++ Lab9/L9_0.Generic Box/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L9_0.Generic_Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var box1 = new Box<int>(123123);
+            var box2 = new Box<string>("life in a box");
+
+            Console.WriteLine(box1);
+            Console.WriteLine(box2);
+
+            Console.ReadKey();
+        }
+    }
+}
