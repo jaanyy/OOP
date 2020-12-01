30c5d5c0c8564988ebb4b378d232074bb206ec9e
jany <janaak827@gmail.com>
2020-12-01 22:32:29 +0200

    L9_1.Generic Box of String

---
+++ Lab9/L9_1.Generic Box of String/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L9_1.Generic_Box_of_String
+{
+    class Box<T>
+    {
+        private T value;
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}";
+        }
+    }
+}
---
+++ Lab9/L9_1.Generic Box of String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace L9_1.Generic_Box_of_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string value = Console.ReadLine();
+                Box<string> box = new Box<string>(value);
+
+                Console.WriteLine(box);
+            }
+            Console.ReadKey();
+        }
+    }
+}
