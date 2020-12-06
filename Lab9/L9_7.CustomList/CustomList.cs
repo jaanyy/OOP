d46268cbd3689280ba7e51209e4a685ec2bb36a1
jany <janaak827@gmail.com>
2020-12-07 01:47:13 +0200

    L9_7.CustomList

---
+++ Lab9/L9_7.CustomList/CustomList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L9_7.CustomList
+{
+    class CustomList<T>
+        where T : IComparable<T>
+    {
+        public List<T> Data;
+
+        public CustomList()
+        {
+            Data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            Data.Add(element);
+        }
+
+        public T Remove(int index)
+        {
+            T element = Data[index];
+            Data.RemoveAt(index);
+
+            return element;
+        }
+
+        public bool Contains(T element)
+        {
+            return Data.Contains(element);
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            T temp = Data[index1];
+            Data[index1] = Data[index2];
+            Data[index2] = temp;
+        }
+
+        public int CountGreaterThan(T element)
+        {
+            return Data.Count(e => e.CompareTo(element) > 0);
+        }
+
+        public T Max()
+        {
+            return Data.Max();
+        }
+
+        public T Min()
+        {
+            return Data.Min();
+        }
+
+    }
+}
---
+++ Lab9/L9_7.CustomList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace L9_7.CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new CustomList<string>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+
+                switch (input[0])
+                {
+                    case "Add":
+                        {
+                            list.Add(input[1]);
+                            break;
+                        }
+                    case "Remove":
+                        {
+                            list.Remove(Convert.ToInt32(input[1]));
+                            break;
+                        }
+                    case "Contains":
+                        {
+                            Console.WriteLine(list.Contains(input[1]));
+                            break;
+                        }
+                    case "Swap":
+                        {
+                            list.Swap(Convert.ToInt32(input[1]), Convert.ToInt32(input[2]));
+                            break;
+                        }
+                    case "Greater":
+                        {
+                            Console.WriteLine(list.CountGreaterThan(input[1]));
+                            break;
+                        }
+                    case "Max":
+                        {
+                            Console.WriteLine(list.Max());
+                            break;
+                        }
+                    case "Min":
+                        {
+                            Console.WriteLine(list.Min());
+                            break;
+                        }
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
