09a42e46b90061672105504f37cba89ddd729b71
jany <janaak827@gmail.com>
2020-12-09 22:59:40 +0200

    L15_3_StudentsByAge

---
+++ Lab15/L15_3_StudentsByAge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_3_StudentsByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Student(input[0], input[1], Convert.ToInt32(input[2])));
+            }
+            var result = StudentByAge(students);
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName} {r.Age}");
+            }
+
+            Console.ReadKey();
+        }
+        static List<Student> StudentByAge(List<Student> studs)
+        {
+            var result = studs.Where(s => s.Age >= 18 && s.Age <= 24).ToList();
+
+            return result;
+        }
+    }
+}
---
+++ Lab15/L15_3_StudentsByAge/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_3_StudentsByAge
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public Student(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+            Age = age;
+        }
+        
+    }
+}
