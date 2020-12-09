ed85ca77ea8a672b6c696b4ca48667383fed576f
jany <janaak827@gmail.com>
2020-12-09 19:28:02 +0200

    L9_10.Tuple

---
+++ Lab9/L9_10.Tuple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L9_10.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            Tuple<string, string> tuple1 = new Tuple<string, string>(input[0] + " " + input[1], input[2]);
+            Console.WriteLine(tuple1);
+
+            input = Console.ReadLine().Split();
+            Tuple<string, int> tuple2 = new Tuple<string, int>(input[0], Convert.ToInt32(input[1]));
+            Console.WriteLine(tuple2);
+
+            input = Console.ReadLine().Split();
+            Tuple<int, double> tuple3 = new Tuple<int, double>(Convert.ToInt32(input[0]), Convert.ToDouble(input[1].Replace('.',',')));
+            Console.WriteLine(tuple3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab9/L9_10.Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L9_10.Tuple
+{
+    class Tuple<T1, T2>
+    {
+        public T1 Item1 { get; }
+        public T2 Item2 { get; }
+
+        public Tuple(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
