a5138be6d12996fe621162c324fa5816fac1de2d
jany <janaak827@gmail.com>
2020-12-09 21:58:58 +0200

    L15_1_StudentByGroup

---
+++ Lab15/L15_1_StudentsByGroup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_1_StudentsByGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Student(input[0], input[1], Convert.ToInt32(input[2])));
+            }
+            var result = students.Where(s => s.GroupNumber == 2)
+                                 .OrderBy(s => s.FirstName);
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_1_StudentsByGroup/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_1_StudentsByGroup
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int GroupNumber { get; set; }
+        public Student(string firstName, string lastName, int group)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+            GroupNumber = group;
+        }
+    }
+}
