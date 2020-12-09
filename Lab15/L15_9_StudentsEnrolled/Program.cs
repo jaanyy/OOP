a9fbce433d69f49ac93809655e4f616b26f7f038
jany <janaak827@gmail.com>
2020-12-10 00:41:15 +0200

    L15_9_StudentsEnrolled

---
+++ Lab15/L15_9_StudentsEnrolled/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_9_StudentsEnrolled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                List<int> marks = new List<int>();
+                for (int i = 1; i < input.Length; i++)
+                {
+                    marks.Add(Convert.ToInt32(input[i]));
+                }
+
+                students.Add(new Student(input[0], marks));
+            }
+
+            var result = students.Where(s => s.FacultyNumber.EndsWith("14") || s.FacultyNumber.EndsWith("15"));
+
+            foreach (var r in result)
+            {
+                foreach(var m in r.Marks)
+                {
+                    Console.Write($"{m} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_9_StudentsEnrolled/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_9_StudentsEnrolled
+{
+    class Student
+    {
+        public string FacultyNumber { get; set; }
+        public List<int> Marks { get; set; }
+        public Student(string facultyNumber,  List<int> marks)
+        {
+            FacultyNumber = facultyNumber;
+            Marks = marks;
+        }
+    }
+}
