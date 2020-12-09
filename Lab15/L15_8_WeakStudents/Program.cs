5747414264f8fb0d0a7c96c694ccfc16fd1fe136
jany <janaak827@gmail.com>
2020-12-10 00:31:50 +0200

    L15_8_WeakStudents

---
+++ Lab15/L15_8_WeakStudents/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_8_WeakStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                List<int> marks = new List<int>();
+                for (int i = 2; i < input.Length; i++)
+                {
+                    marks.Add(Convert.ToInt32(input[i]));
+                }
+
+                students.Add(new Student(input[0], input[1], marks));
+            }
+
+            var result = students.Where(s => s.Marks.Count(m => m <= 3) >= 2);
+
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_8_WeakStudents/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_8_WeakStudents
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public List<int> Marks { get; set; }
+        public Student(string firstName, string lastName, List<int> marks)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+            Marks = marks;
+        }
+    }
+}
