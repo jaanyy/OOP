14d55df97786f8cd78bdd5e1ba3eee5fb6a8ee88
jany <janaak827@gmail.com>
2020-12-09 22:18:41 +0200

    L15_2_StudentsByFirstAndLastName

---
+++ Lab15/L15_2_StudentsByFirstAndLastName/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_2_StudentsByFirstAndLastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Student(input[0], input[1]));
+            }
+            var result = students.Where(s => s.FirstName.CompareTo(s.SecondName) < 0);
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_2_StudentsByFirstAndLastName/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_2_StudentsByFirstAndLastName
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+        }
+    }
+}
