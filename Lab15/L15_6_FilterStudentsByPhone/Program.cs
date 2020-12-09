4078850df3e92eb34f4c68101a2bfd091de605cb
jany <janaak827@gmail.com>
2020-12-10 00:05:28 +0200

    L15_6_FilterStudentsByPhone

---
+++ Lab15/L15_6_FilterStudentsByPhone/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_6_FilterStudentsByPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Student(input[0], input[1], input[2]));
+            }
+            var result = students.Where(s => s.Phone.StartsWith("02") || s.Phone.StartsWith("+3592"));
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_6_FilterStudentsByPhone/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_6_FilterStudentsByPhone
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public Student(string firstName, string lastName, string phone)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+            Phone = phone;
+        }
+    }
+}
