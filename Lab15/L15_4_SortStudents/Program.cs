14b35badeabab9353b4ea49f7f3e836cfde67587
jany <janaak827@gmail.com>
2020-12-09 23:23:27 +0200

    L15_4_SortStudents

---
+++ Lab15/L15_4_SortStudents/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace L15_4_SortStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Student(input[0], input[1]));
+            }
+            var result = students.OrderBy(s => s.SecondName)
+                                 .ThenByDescending(s => s.FirstName);
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_4_SortStudents/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_4_SortStudents
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+        }
+    }
+}
