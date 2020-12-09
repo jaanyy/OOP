cae4c6ec62bd7592454811623d4414d52d7be41f
jany <janaak827@gmail.com>
2020-12-09 23:51:05 +0200

    L15_5

---
+++ Lab15/L15_5_FilterStudentsByEmailDomain/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_5_FilterStudentsByEmailDomain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Student(input[0], input[1], input[2]));
+            }
+            var result = students.Where(s => s.Email.EndsWith("@gmail.com"));
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_5_FilterStudentsByEmailDomain/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_5_FilterStudentsByEmailDomain
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public Student(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+            Email = email;
+        }
+    }
+}
