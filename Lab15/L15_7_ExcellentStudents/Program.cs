2bd716bc50a390d68a05712858d91e27f8148ffe
jany <janaak827@gmail.com>
2020-12-10 00:17:26 +0200

    L15_7_ExcellentStudents

---
+++ Lab15/L15_7_ExcellentStudents/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_7_ExcellentStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                List<int> marks = new List<int>();
+                for (int i = 2; i < input.Length; i++)
+                {
+                    marks.Add(int.Parse(input[i]));
+                }
+                    
+                students.Add(new Student(input[0], input[1], marks));
+            }
+            var result = students.Where(s => s.Marks.Any(m => m == 6));
+            foreach(var r in result)
+            {
+                Console.WriteLine($"{r.FirstName} {r.SecondName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab15/L15_7_ExcellentStudents/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_7_ExcellentStudents
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public List<int> Marks { get; set; }
+        public Student(string firstName, string lastName, List<int> marks)
+        {
+            FirstName = firstName;
+            SecondName = lastName;
+            Marks = marks;
+        }
+    }
+}
