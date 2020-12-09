12246f946b9eea7cc0cae7b1c1e91abbfc4e6e6a
jany <janaak827@gmail.com>
2020-12-10 01:06:12 +0200

    L15_10_GroupByGroup

---
+++ Lab15/L15_10_GroupByGroup/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L15_10_GroupByGroup
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Group { get; set; }
+        public Person(string name, int group)
+        {
+            Name = name;
+            Group = group;
+        }
+    }
+}
---
+++ Lab15/L15_10_GroupByGroup/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_10_GroupByGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> students = new List<Person>();
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split();
+                students.Add(new Person(input[0]+input[1], Convert.ToInt32(input[2])));
+            }
+
+            var result = students.OrderBy(s => s.Group).GroupBy(s => s.Group);
+
+            foreach (var r in result)
+            {
+                Console.Write($"{r.Key} - ");
+                foreach (var g in r)
+                {
+                    Console.Write($"{g.Name}");
+                    if (g != r.Last())
+                    {
+                        Console.Write(", ");
+                    }
+                        
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
