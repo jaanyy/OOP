87e170363ae5cc586efe0670103ef70720e0ad8b
jany <janaak827@gmail.com>
2020-12-10 02:08:00 +0200

    L15_12_Office

---
+++ Lab15/L15_12_OfficeStuff/Company.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L15_12_OfficeStuff
+{
+    class Company
+    {
+        string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.All(x => char.IsLetter(x)) && value.Length <= 20)
+                    _name = value;
+                else
+                    throw new Exception("Invalid company name!");
+            }
+        }
+        public Dictionary<string, int> Products;
+        public Company(string name)
+        {
+            Name = name;
+            Products = new Dictionary<string, int>();
+        }
+        public void AddProduct(string name, int amount)
+        {
+            if (name.All(x => char.IsLetter(x)) && name.Length <= 20)
+            {
+                if (Products.ContainsKey(name) && amount > 0)
+                {
+                    if (Products[name] + amount > 1000)
+                        throw new Exception("Invalid amount!");
+                    else
+                        Products[name] += amount;
+                }
+                else if (amount > 0 && amount <= 1000)
+                {
+                    Products.Add(name, amount);
+                }
+                else
+                {
+                    throw new Exception("Invalid amount!");
+                }
+            }
+            else
+                throw new Exception("Invalid product name!");
+
+        }
+        public override string ToString()
+        {
+            string info = $"{Name}: ";
+            foreach (var p in Products)
+            {
+                info += $"{p.Key}-{p.Value}";
+                if (p.Equals(Products.Last()) == false)
+                    info += ", ";
+            }
+            return info;
+        }
+    }
+}
---
+++ Lab15/L15_12_OfficeStuff/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L15_12_OfficeStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n >= 1 && n <= 100)
+                {
+                    Dictionary<string, Company> companies = new Dictionary<string, Company>();
+                    char[] chars = { ' ', '-', '|' };
+                    for (int i = 0; i < n; i++)
+                    {
+                        string[] input = Console.ReadLine().Split(chars, StringSplitOptions.RemoveEmptyEntries);
+                        if (companies.ContainsKey(input[0]))
+                        {
+                            try
+                            {
+                                companies[input[0]].AddProduct(input[2], Convert.ToInt32(input[1]));
+                            }
+                            catch (Exception x)
+                            {
+                                Console.WriteLine(x.Message);
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                Company t = new Company(input[0]);
+                                t.AddProduct(input[2], Convert.ToInt32(input[1]));
+                                companies.Add(input[0], t);
+                            }
+                            catch (Exception x)
+                            {
+                                Console.WriteLine(x.Message);
+                            }
+                        }
+                    }
+                    foreach (var company in companies.OrderBy(x => x.Key))
+                        Console.WriteLine(company.Value);
+                }
+                else
+                    throw new Exception("Invalid number of commands!");
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
