40301ec277888cff408d26401973a0b8ff27f54e
jany <janaak827@gmail.com>
2020-12-13 22:36:28 +0200

    L13_4_ReplaceTag

---
+++ Lab13/L13_4_ReplaceTag/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace L13_4_ReplaceTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"<a");
+            string command = "";
+            string input = "";
+            while ((command = Console.ReadLine()) != "end")
+            {
+                input += command;
+            }
+            string result = regex.Replace(input, $"[URL ");
+            result = result.Replace("</a>", "[/URL]");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
