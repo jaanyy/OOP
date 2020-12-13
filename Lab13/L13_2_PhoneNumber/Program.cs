c374829ce03af9be5f9019df9923e4a2ff1fc26d
jany <janaak827@gmail.com>
2020-12-13 20:59:17 +0200

    L13_2_PhoneNumber

---
+++ Lab13/L13_2_PhoneNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace L13_2_PhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            List<string> result = new List<string>();
+            Regex regex = new Regex(@"\+359([ ]2[ ]\d{3}[ ]\d{4}|[-]2[-]\d{3}[-]\d{4})\b");
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    input += command + " , ";
+                }
+            }
+            MatchCollection matches = regex.Matches(input);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no matches");
+            }
+            Console.ReadKey();
+        }
+    }
+}
