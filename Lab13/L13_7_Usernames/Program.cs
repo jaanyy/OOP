6accb52343fdc46722f0f5dba53e867d6dece714
jany <janaak827@gmail.com>
2020-12-14 01:23:12 +0200

    -

---
+++ Lab13/L13_7_Usernames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L13_7_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://regex101.com/r/6MgcW1/1
+        }
+    }
+}
