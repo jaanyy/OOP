7f9b47dcf8c21862bfa032c2fbbbaba3a55a86d2
jany <janaak827@gmail.com>
2020-12-14 00:35:46 +0200

    L13_6

---
+++ Lab13/L13_6_Sentence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace L13_6_Sentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex key = new Regex($@"\b{Console.ReadLine()}\b");
+            Regex regex = new Regex(@"\b.+?[?!.]");
+            foreach (var i in regex.Matches(Console.ReadLine()))
+                if (key.IsMatch(i.ToString()))
+                    Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
