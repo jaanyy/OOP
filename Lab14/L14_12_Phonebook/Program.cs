b15bef94fc993b95bb1ac3685f05196d55cba22b
jany <janaak827@gmail.com>
2020-12-14 03:09:23 +0200

    L14_12

---
+++ Lab14/L14_12_Phonebook/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L14_12_Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                if (input[0] == "END")
+                {
+                    break;
+                }   
+                if (input[0] == "A")
+                {
+                    if (phoneBook.ContainsKey(input[1]))
+                    {
+                        phoneBook[input[1]] = input[2];
+                    }   
+                    else
+                    {
+                        phoneBook.Add(input[1], input[2]);
+                    }     
+                }
+                if (input[0] == "S")
+                {
+                    if (phoneBook.ContainsKey(input[1]))
+                    {
+                        Console.WriteLine($"{input[1]} -> {phoneBook[input[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
