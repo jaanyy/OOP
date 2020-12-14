0346efaaff8257b152c3c25e207cee99fe38d3b4
jany <janaak827@gmail.com>
2020-12-14 02:11:53 +0200

    14_3

---
+++ Lab14/L14_3_Censor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace L14_3_Censor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = Console.ReadLine();
+            string[] data = email.Split('@');
+            string username = data[0];
+            string domain = data[1];
+            string censor = "";
+
+            Regex regex = new Regex(@"(\S)");
+            censor = regex.Replace(username, "*");
+
+            string censoredEmail = censor + "@" + domain;
+
+            string text = Console.ReadLine();
+
+            text = text.Replace(email, censoredEmail);
+            Console.WriteLine(text);
+
+            Console.ReadKey();
+        }
+    }
+}
