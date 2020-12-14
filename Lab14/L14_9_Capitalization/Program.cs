3a049fd1ec4d8f08fb5a4bd261c3428a9716710c
jany <janaak827@gmail.com>
2020-12-14 02:57:03 +0200

    L14_9

---
+++ Lab14/L14_9_Capitalization/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L14_9_Capitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = { ' ', '.', ',', '?', '!', ';' };
+            string[] words = Console.ReadLine().Split(chars);
+            var result = words.Select(w => char.ToUpper(w[0]) + w[1..]).ToArray();
+            foreach(var r in result)
+            {
+                Console.Write($"{r} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
