1ad82aae36ec8ce00a664f80096cdf0e96f59b04
jany <janaak827@gmail.com>
2020-12-14 02:23:11 +0200

    L14_4

---
+++ Lab14/L14_4_Keyword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace L14_4_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyword = Console.ReadLine();
+            string text = Console.ReadLine();
+            char[] chars = { '.', '!', '?' };
+            string[] sentences = text.Split(chars);
+            int n = sentences.Length;  
+            for (int i = 0; i < n; i++)
+            {
+                string[] words = sentences[i].Split(" ");
+                bool isFound = false;
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (words[j] == keyword)
+                    {
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                    Console.WriteLine(sentences[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
