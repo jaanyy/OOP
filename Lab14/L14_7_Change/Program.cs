b908a01603951a2e931138af3244223eb22f80af
jany <janaak827@gmail.com>
2020-12-14 02:34:58 +0200

    L14_7

---
+++ Lab14/L14_7_Change/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace L14_7_Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length - 7; i++)
+            {
+                if (text.Substring(i, 8) == "<upcase>")
+                {
+                    for (int j = i; j < text.Length - 8; j++)
+                    {
+                        if (text.Substring(j, 9) == "</upcase>")
+                        {
+                            string textToEdit = text.Substring(i + 8, j - i - 8);
+                            textToEdit = textToEdit.ToUpper();
+                            text = text.Remove(i, j - i + 9);
+                            text = text.Insert(i, textToEdit);
+                            break;
+                        }
+                    }
+                        
+                }
+            }
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
