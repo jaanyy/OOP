d50d3f2d6c45462f86ac253244307ebca8a0dac5
jany <janaak827@gmail.com>
2020-12-14 03:04:35 +0200

    L14_11

---
+++ Lab14/L14_11_CommonString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L14_11_CommonString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string row1 = Console.ReadLine();
+            string row2 = Console.ReadLine();
+            bool isCommon = false;
+            for (int i = 1; i <= row1.Length; i++)
+            {
+                for (int j = 0; j < row1.Length - i + 1; j++)
+                    if (row2.Contains(row1.Substring(j, i)))
+                    {
+                        isCommon = true;
+                        break;
+                    }
+            }
+            if (isCommon)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}
