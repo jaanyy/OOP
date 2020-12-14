4ba80ab38ec49241d9265e6f2c5eb531f8df2bb8
jany <janaak827@gmail.com>
2020-12-14 03:02:07 +0200

    L14_10

---
+++ Lab14/L14_10_Palindrome_Index/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace L14_10_Palindrome_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine().ToLower();
+            int result = 0;
+            if (a == new string(a.ToCharArray().Reverse().ToArray()))
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    string b = a.Remove(i);
+                    if (b == new string(b.ToCharArray().Reverse().ToArray()))
+                    {
+                        result = i;
+                    }
+                        
+                }
+                Console.WriteLine(result);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
