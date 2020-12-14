bac1c0d0a03cfc7a463415ed7ff8838021788428
jany <janaak827@gmail.com>
2020-12-14 02:28:55 +0200

    L14_5

---
+++ Lab14/L14_5_URL/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace L14_5_URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = Console.ReadLine();
+            string protocol = "";
+            string server = "";
+            string resource = "";
+            int k = url.IndexOf("://");
+            if (k < 0)
+            {
+                protocol = "";
+            }   
+            else
+            {
+                protocol = url.Substring(0, k);
+                url = url.Substring(k + 3, url.Length - (k + 3));
+            }
+            k = url.IndexOf("/");
+            if (k < 0)
+            {
+                server = url;
+                resource = "";
+            }
+            else
+            {
+                server = url.Substring(0, k);
+                resource = url.Substring(k + 1, url.Length - (k + 1));
+            }
+            Console.WriteLine($"[protocol] = \"{protocol}\"\n[server] = \"{server}\"\n[resource] = \"{resource}\"");
+
+            Console.ReadKey();
+        }
+    }
+}
