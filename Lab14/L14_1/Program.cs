ad496edb3e34eba66f69e99b77659b3dd567592f
jany <janaak827@gmail.com>
2020-12-14 02:02:37 +0200

    L14_1

---
+++ Lab14/L14_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace L14_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string r = new string(input.ToCharArray().Reverse().ToArray());
+            Console.Write($"Variant I: {r}\n");
+            Console.Write("Variant II: ");
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{input[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
